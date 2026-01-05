8f4450981168e542d68fb4a7ddc3f057515d4986
ankurdotio <prajapatiankur8349@gmail.com>
2026-01-06 01:37:46 +0530

    Add initial HTML and JavaScript files for video-4.0 sub-module

---
+++ module-3.0/sub-module-3.1/video-4.0/script.js
@@ -0,0 +1,13 @@
+/* Agar ye toh kuch krna hai , ya phir kuch aur krna hai */
+
+const user = { age: 20 }
+
+
+/* Truthy and Falsy */
+
+
+if (false) {
+    console.log("user is older")
+} else if (false) {
+
+}
