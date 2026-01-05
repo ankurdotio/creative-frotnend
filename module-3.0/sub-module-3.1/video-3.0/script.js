de906d089e8d6ea67fb850b491f0783f14c384f0
ankurdotio <prajapatiankur8349@gmail.com>
2026-01-06 00:57:47 +0530

    Add initial HTML and JavaScript files for video-3.0 sub-module

---
+++ module-3.0/sub-module-3.1/video-3.0/script.js
@@ -0,0 +1,21 @@
+/* Data types => data ke types */
+
+
+/* 
+
+firstname = Ankur
+    lastname = verma
+    age = 35
+    gender : Male [MALE/FEMALE]
+ */
+
+
+/* 
+
+PRIMITIVE => Number,String,Boolean,Undefined,Null
+REFERENCE => Object {},array [],function ()
+
+*/
+
+let a = [ 10, 20, 30 ]
+let z = a
